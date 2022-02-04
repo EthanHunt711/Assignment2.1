a55c23d00d7ae4abe4c516c482167b22b51c6a7a
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2022-02-04 12:40:03 +0100

    Linear, Binary, main, get_random done

---
+++ main.py
@@ -0,0 +1,22 @@
+from binary_search import binary_search
+from linear_search import linear_search
+from bubble_sort import bubble_sort
+from get_random import lists
+import time
+
+if __name__ == "__main__":
+    something_start = time.time()
+    something_l = linear_search(lists[0], 5, 5)
+    something_stop = time.time()
+    something_running_time_linear = something_stop - something_start
+
+    something_start = time.time()
+    something_b = binary_search(sorted(lists[0]), 5)
+    something_stop = time.time()
+    something_running_time_binary = something_stop - something_start
+
+    print(f'The running time for Linear Search is: {something_running_time_linear}')
+    print(f'The serach result is: {something_l}')
+    print('---------------------------------------------------------------------------')
+    print(f'The running time for Binary Search is: {something_running_time_binary}')
+    print(f'The search result is: {something_b}')
