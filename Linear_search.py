d56b029dfb7ffa7b69735d0bdd6c0906b5439a96
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2022-02-04 11:13:30 +0100

    Linear done

--- Linear_search.py
+++ Linear_search.py
@@ -1,5 +1,13 @@
-def search(arr, x):
-    for i in range(len(arr)):
-        if arr[i] == x:
-            return i
-    return -1
+def linear_search(l_nrs, target_x, idx=0):
+    try:
+        assert all(type(item) == int for item in l_nrs) is True
+    except AssertionError:
+        print('There is an invalid item in the list')
+    else:
+        if len(l_nrs) > 0:
+            if all(type(item) == int for item in l_nrs) is True:
+                if target_x >= 0:
+                    if idx >= 0:
+                        if l_nrs[idx] == target_x:
+                            return True
+        return False
