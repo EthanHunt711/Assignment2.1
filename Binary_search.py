72e5293d37c27c2ae3c9fa03957553464355aca6
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2022-02-04 11:49:53 +0100

    Linear and Binary done

--- Binary_search.py
+++ Binary_search.py
@@ -1,13 +1,18 @@
-def search(arr, i):
-    n = len(arr)
-    left = 0
-    right = n-1
-    while left <= right:
-        middle = (left + right) // 2
-        if i < arr[middle]:
-            right = middle-1
-        elif i > arr[middle]:
-            left = middle+1
-        else:
-            return middle
-    return -1
+def binary_search(l_sorted, target_x):
+    try:
+        assert l_sorted == sorted(l_sorted)
+    except AssertionError:
+        print('The input list is not sorted')
+    else:
+        n = len(l_sorted)
+        left = 0
+        right = n-1
+        while left <= right:
+            middle = (left + right) // 2
+            if target_x < l_sorted[middle]:
+                right = middle-1
+            elif target_x > l_sorted[middle]:
+                left = middle+1
+            else:
+                return True
+        return False
