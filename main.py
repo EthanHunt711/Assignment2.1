d66deeea9eb61ee73f7753596f11660ca26f7ee4
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2022-02-05 00:00:57 +0100

    bubble_search remaining

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ from get_random import lists
 import time
 
 
-"""The main program for implementing the algorithms and saving the running time"""
-if __name__ == "__main__":
+def main(random_lists, list_index):
+
     something_start = time.time()
-    something_l = linear_search(lists[0], 5, 5)
+    something_l = linear_search(random_lists[list_index], 5, 5)
     something_stop = time.time()
     something_running_time_linear = something_stop - something_start
 
     something_start = time.time()
-    something_b = binary_search(sorted(lists[0]), 5)
+    something_b = binary_search(sorted(random_lists[list_index]), 5)
     something_stop = time.time()
     something_running_time_binary = something_stop - something_start
 
@@ -22,3 +22,8 @@ if __name__ == "__main__":
     print('---------------------------------------------------------------------------')
     print(f'The running time for Binary Search is: {something_running_time_binary}')
     print(f'The search result is: {something_b}')
+
+
+"""The main program for implementing the algorithms and saving the running time"""
+if __name__ == "__main__":
+    main(lists, 0)
