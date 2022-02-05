732fee1fa05a5aa4e96954fae5b8fc38891e4e41
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2022-02-05 10:12:30 +0100

    main change

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import sys
+import csv
+
 from binary_search import binary_search
 from linear_search import linear_search
 from bubble_sort import bubble_sort
+
 from get_random import lists
 import time
 
 
-def main(random_lists, list_index):
+def main(random_lists, list_index=0):
 
     something_start = time.time()
     something_l = linear_search(random_lists[list_index], 5, 5)
@@ -23,7 +27,25 @@ def main(random_lists, list_index):
     print(f'The running time for Binary Search is: {something_running_time_binary}')
     print(f'The search result is: {something_b}')
 
+    # header = ['Search Algorithm', 'Running Time', 'Search Result']
+    #
+    # with open(sys.stdout, 'w') as out_file:
+    #
+    #     writer = csv.writer(out_file)
+    #     writer.writeheader(header)
+    #
+    #     writer.writerow(main(lists, 0))
+    #
+    #     out_file.close()
 
-"""The main program for implementing the algorithms and saving the running time"""
+
+"""The main program for implementing the algorithms and saving the running time
+in a .csv file"""
 if __name__ == "__main__":
-    main(lists, 0)
+    n = 1
+    for list in lists:
+        print(f'Search reults for list {n}')
+        main(lists, 0)
+        print('---------------------------------------')
+        print('---------------------------------------')
+        n += 1
