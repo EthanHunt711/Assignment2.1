781652b8b76251b39ec65b801bb1c43f27a67541
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2022-02-05 21:44:49 +0100

    recursion implemented

--- binary_search.py
+++ binary_search.py
@@ -9,12 +9,14 @@ def binary_search(l_sorted, target_x):
         n = len(l_sorted)
         left = 0  # the left bound
         right = n-1  # the right bound
-        while left <= right:  # checking whether the left and right bound collide
+        if right >= left:
             middle = (left + right) // 2  # dividing the list for faster searching
-            if target_x < l_sorted[middle]:  # if the target value is in the left half
-                right = middle-1  # alter the right bound
-            elif target_x > l_sorted[middle]:  # if the target value is in the right half
-                left = middle+1  # alter the left bound
-            else:
-                return True  # if the target value is in the list
+            if target_x == l_sorted[middle]:  # conditioning the left and right bound collide
+                return True
+            elif target_x > l_sorted[middle]:  # if the target value is in the left half
+                left = middle+1  # alter the right bound
+                return binary_search(l_sorted[middle+1:], target_x)
+            else:  # if the target value is in the right half
+                right = middle-1  # alter the left bound
+                return binary_search(l_sorted[:middle], target_x)
         return False   # if the target value is not in the list
--- get_random.py
+++ get_random.py
@@ -6,23 +6,23 @@ from random import randint
 #     l_1.append(n)
 
 l_2 = []
-for i in range(0, 10):
-    n = randint(1, 100)
+for i in range(0, 100):
+    n = randint(1, 10000)
     l_2.append(n)
 
 l_3 = []
-for i in range(0, 10):
-    n = randint(1, 100)
+for i in range(0, 100):
+    n = randint(1, 10000)
     l_3.append(n)
 
 l_4 = []
-for i in range(0, 10):
-    n = randint(1, 100)
+for i in range(0, 100):
+    n = randint(1, 10000)
     l_4.append(n)
 
 l_5 = []
-for i in range(0, 10):
-    n = randint(1, 100)
+for i in range(0, 100):
+    n = randint(1, 10000)
     l_5.append(n)
 
 lists = [l_2, l_3, l_4, l_5]
