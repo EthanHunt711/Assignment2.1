c9ef6a727011787b972e61aa6812f9f6969e8e81
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2022-03-04 11:03:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-from binary_search import binary_search
-from linear_search import linear_search
-from bubble_sort import bubble_sort
+from Linear_search import linear_search
+from Binary_search import binary_search
+from Bubble_sort import bubble_sort
 
-from get_random import lists
+from get_random import *
 import time
 
 
 def main(random_lists, list_index=0):
     # the running time for the linear search
     something_start = time.time()
-    something_l = linear_search(random_lists[list_index], 5)
+    something_l = linear_search(random_lists, list_index)
     something_stop = time.time()
     something_running_time_linear = something_stop - something_start
 
     # the running time for the binary search
     something_start = time.time()
-    something_b = binary_search(sorted(random_lists[list_index]), 5)
+    something_b = binary_search(sorted(random_lists), list_index)
     something_stop = time.time()
     something_running_time_binary = something_stop - something_start
 
     # the running time for the bubble sort
     something_start = time.time()
-    something_bl = bubble_sort(random_lists[list_index])
+    something_bl = bubble_sort(random_lists)
     something_stop = time.time()
     something_running_time_bubble = something_stop - something_start
 
@@ -36,10 +36,13 @@ def main(random_lists, list_index=0):
 could be saved in a .csv file if it is run in the terminal"""
 
 if __name__ == "__main__":
+    lists = []
+    for i in range(10):
+        lists.append(get_random(0, 994))  # maximum recursion is 994
     n = 0
-    for list in lists:  # looping through the lists since the data is a list of random lists
-        print(f'Search reults for list {n+1} with {len(list)} elements')
-        main(lists, n)
+    for lists_w in lists:  # looping through the lists since the data is a list of random lists
+        print(f'Search reults for list {n+1} with {len(lists_w)} elements')
+        main(lists_w, n)
         print('---------------------------------------')
         print('---------------------------------------')
         n += 1
